6cc8a4d0b077a8a389a3a00243abe76e6b0539b4
Karine Xavier Silva <kariinexavier@gmail.com>
2020-04-01 15:25:41 -0300

    criando comidinha

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ snake[0]={
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG(){
     context.fillStyle = 'lightgreen';
     context.fillRect(0,0,16*box,16*box);
@@ -16,6 +21,11 @@ function criarCobrinha(){
         context.fillStyle="green";
         context.fillRect(snake[i].x,snake[i].y,box,box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x,food.y,box,box);
 }
 
 document.addEventListener('keydown', update);
@@ -34,6 +44,7 @@ function iniciarJogo(){
     if(snake[0].y<0 && direction == "up") snake[0].y= 16*box;
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
